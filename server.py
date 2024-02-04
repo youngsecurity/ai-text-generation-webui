17330b39c60c18a0bf5f8772e1f0428d8ce025c9
Joseph Young <130690493+yousecjoe@users.noreply.github.com>
2024-02-03 19:23:19 -0500

    create a public link

--- server.py
+++ server.py
@@ -161,7 +161,8 @@ def create_interface():
     with OpenMonkeyPatch():
         shared.gradio['interface'].launch(
             prevent_thread_lock=True,
-            share=shared.args.share,
+            #share=shared.args.share,
+            share=True,
             server_name=None if not shared.args.listen else (shared.args.listen_host or '0.0.0.0'),
             server_port=shared.args.listen_port,
             inbrowser=shared.args.auto_launch,
